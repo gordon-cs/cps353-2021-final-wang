411594477ce08d12c5a6e72ef3c09a27613a97e9
Eric Wang <Eric.Wang@gordon.edu>
2021-12-02 21:05:35 -0500

    PRE NEXT page function

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ export default class App extends Component{
   state = {data: null};
 
   componentDidMount(){
-    this.getData('Genesis',1,1);
+    this.getData('Hebrews',12,1);
   }
 
-  getData(book,chapter,verse){
-    Axios.get(this.url+book+"%20"+chapter+':'+verse+"&type=json").then((abc) => {
-      console.log(abc);
+  getData(book,chapter,verse) {
+    if(verse===null) {
+      Axios.get(this.url+book+"%20"+chapter+"&type=json").then((abc) => {
+        console.log(abc);
 
-      this.setState({ data: abc.data})
-    });
+        this.setState({ data: abc.data})
+      });
+    } else {
+      Axios.get(this.url+book+"%20"+chapter+':'+verse+"&type=json").then((abc) => {
+        console.log(abc);
+
+        this.setState({ data: abc.data})
+      });
+    }
   }
   
   showContent(){
@@ -33,22 +41,78 @@ export default class App extends Component{
     });
   }
   
+  // showButton(index){
+  //   if(true) {
+  //     return  (<button id="front" class="textBox" onClick={()=> this.getData(this.state.data[0].bookname,
+  //     this.state.data[0].chapter-1,
+  //     this.state.data[0].verse)}> 
+  //     index
+  //     </button>);
+      
+  //   } else {
+  //     return
+  //      <button id="front" class="textBox" > 
+  //     index
+  //     </button>;
+  //   }
+  // }
+
+  showPre(){
+    if(true) {
+      return  (<button id="pre" class="textBox" onClick={()=> this.getData(this.state.data[0].bookname,
+      this.state.data[0].chapter-1,
+      this.state.data[0].verse)}> 
+      PRE
+      </button>);
+      
+    } else {
+      return
+       <button id="front" class="textBox" > 
+      NEXT
+      </button>;
+    }
+  }
+
+  showNext(){
+    if(true) {
+      return  (<button id="next" class="textBox" onClick={()=> this.getData(this.state.data[0].bookname,
+      this.state.data[0].chapter-(-1),
+      this.state.data[0].verse)}> 
+      NEXT
+      </button>);
+      
+    } else {
+      return
+       <button id="front" class="textBox" > 
+      NEXT
+      </button>;
+    }
+  }
+  
   render(){
     if (!this.state.data) return<div>Sorry can't find the verse.</div>;
 
     return (
       <div class="main">
         <div class="text"> 
-          <input type="text" id="bookName" class="textBox"/>
-          <span> </span>
-          <input type="text" id="chapter" class="textBox" />
-          <span>:</span>
-          <input type="text" id="verse" class="textBox"/>
+          {this.showPre()}
+            <input type="text" id="bookName" class="textBox"/>
+            <span> </span>
+            <input type="text" id="chapter" class="textBox" />
+            <span>:</span>
+            <input type="text" id="verse" class="textBox"/>
+    
           <button id="search" class="textBox" onClick={()=> this.getData(document.getElementById("bookName").value,
                                             document.getElementById("chapter").value,
                                             document.getElementById("verse").value)}> 
-            search
+          Search
           </button>
+          <button id="front" class="textBox" onClick={()=> this.getData(this.state.data[0].bookname,
+          this.state.data[0].chapter,
+          this.state.data[0].verse-5)}> 
+          Full Chapter
+          </button>
+          {this.showNext()}
         </div>
         <div class="content">{this.showContent()}</div>
       </div>
