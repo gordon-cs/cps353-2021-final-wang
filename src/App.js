6213a038dbd34c29351bbb00fe6be5f899c30d59
Eric <Eric@EricMacBookPro.gordon.edu>
2021-12-11 12:54:18 -0500

    fixed the Greeting functino

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import React,{ Component} from 'react';
 import Axios from 'axios';
 
 export default class App extends Component{
-  url = "https://labs.bible.org/api/?passage=";
-  hi = "https://cps353-2021-cloud-wang-5clehebsjq-uc.a.run.app/"
+  constructor(props){
+    super(props);
 
-  state = {data: null,data1: null};
+    this.state = {data: null,data1: null,name: ''};
+
+    this.url = "https://labs.bible.org/api/?passage=";
+    this.hi = "https://cps353-2021-cloud-wang-5clehebsjq-uc.a.run.app";
+
+    this.setUserName = this.setUserName.bind(this);
+  
+    console.log("constructor");
+  }
+ 
+  
 
   componentDidMount(){
-    this.getData('Hebrews',12,1);
-    this.getData1('Eric');
+    this.getVerses('Hebrews',12,1);
+    this.getGreating(this.state.name);
   }
 
-  getData(book,chapter,verse) {
+  getVerses(book,chapter,verse) {
     if(verse.length===0 || verse ==='0') {
       Axios.get(this.url+book+"%20"+chapter+"&type=json").then((abc) => {
         console.log(abc);
@@ -28,14 +38,17 @@ export default class App extends Component{
       });
     }
   }
-  getData1(name){
-    Axios.get(this.hi+name).then((hi) => {
-      console.log(hi);
-
+  getGreating(name){
+    Axios.get(this.hi+'/'+ name).then((hi) => {
+      console.log( hi.data);
       this.setState({ data1: hi.data})
     });
   }
-  
+  showGreeting(){
+    return(
+      <div class="content">{this.state.data1}</div>
+    );
+  }
   showContent(){
     if(this.state.data.length > 1) {
     
@@ -66,7 +79,7 @@ export default class App extends Component{
   //number is the amount of changing page. 
   showButton(buttonName,number){
     if(true) {
-      return  (<button id={buttonName} class="button" onClick={()=> this.getData(this.state.data[0].bookname,
+      return  (<button id={buttonName} class="button" onClick={()=> this.getVerses(this.state.data[0].bookname,
       this.state.data[0].chapter-number,
       '0')}> 
       {buttonName}
@@ -79,6 +92,13 @@ export default class App extends Component{
       </button>;
     }
   }
+
+  setUserName(e) {
+    console.log("changed name to: " + e.target.value);
+    this.setState({name: e.target.value});
+    this.getGreating(e.target.value);
+}
+
   
   render(){
     if (!this.state.data) return<div>Sorry can't find the verse.</div>;
@@ -86,24 +106,23 @@ export default class App extends Component{
     return (
       <div class="main">
         <div class="menu"> 
-            <input type="text" id="userName" class="textBox" placeholder="enter name"/>
-            <span> Enter the scripture you want to look up:</span>
-            <input type="text" id="bookName" class="textBox"/>
+            <input type="text" id="userName" class="textBox" placeholder="enter name" onChange={ this.setUserName } value={ this.state.name } />
+            <input type="text" id="bookName" class="textBox" placeholder="BookName"/>
             <span> </span>
-            <input type="text" id="chapter" class="textBox" />
+            <input type="text" id="chapter" class="textBox" placeholder="chapter"/>
             <span>:</span>
-            <input type="text" id="verse" class="textBox"/>
+            <input type="text" id="verse" class="textBox" placeholder="verse"/>
     
-            <button id="search" class="button" onClick={()=> this.getData(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
+            <button id="search" class="button" onClick={()=> this.getVerses(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
             Search
             </button>
-            <button id="front" class="button" onClick={()=> this.getData(this.state.data[0].bookname,this.state.data[0].chapter,'0')}> 
+            <button id="front" class="button" onClick={()=> this.getVerses(this.state.data[0].bookname,this.state.data[0].chapter,'0')}> 
             Full Chapter
             </button>
-            {this.showButton("PRE",1)}
-            {this.showButton("NEXT",-1)}
-        </div>
-        <div class="hi"> </div>
+        </div >
+        {this.showGreeting()}
+        {this.showButton("PRE",1)}
+        {this.showButton("NEXT",-1)}
         <div class="content">{this.showContent()}</div>
       </div>
     );
