e0138666ec8b4f8c8b79ccd8406341ea761db897
Eric Wang <Eric.Wang@gordon.edu>
2021-12-14 14:35:54 -0500

    Final Version add unerline and flip menu feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component{
   constructor(props){
     super(props);
 
-    this.state = {data: null, greetingData: null, name: '', flip: 'disable'};
+    this.state = {data: null, greetingData: null, name: '', flip: 'disable', count:'3'};
 
     this.url = "https://labs.bible.org/api/?passage=";
     this.hi = "https://cps353-2021-cloud-wang-5clehebsjq-uc.a.run.app";
@@ -40,6 +40,18 @@ export default class App extends Component{
       });
     }
   }
+ 
+  getMultipleVerses(book1,chapter1,verse1,book2,chapter2,verse2) {
+    // Full chapter case
+    
+      Axios.get(this.url+book1+"%20"+chapter1+':'+verse1 + ';' + book2+"%20"+chapter2+':'+verse2 + "&type=json").then((verses) => {
+        console.log(verses);
+
+        this.setState({ data: verses.data})
+      });
+   
+  }
+
   // Call the Google cloud API to get the greeting data
   // @Parameter name is the name of user
   getGreating(name){
@@ -51,12 +63,23 @@ export default class App extends Component{
 
   // Show the greating result on html
   showGreeting(){
-    if (this.state.greetingData == '') return<div>Processing....</div>;
+    if (this.state.greetingData ==='') return<div>Processing....</div>;
 
     return(
-      <div class="content" id="greeting">{this.state.greetingData}</div>
+      <div className="content" id="greeting">{this.state.greetingData}</div>
     );
   }
+
+  // Change the className for each verses to underline it or not
+  changeClassName(key) {
+    const chosen =document.getElementById(key);
+    if(chosen.className === '') {
+      chosen.className = 'chosen';
+    } else {
+      chosen.className = '';
+    }
+  }
+
 
   // Show the bible verses 
   // Base on the parameter verse you previously entered will show multiple or single verses
@@ -65,24 +88,35 @@ export default class App extends Component{
     if(this.state.data.length > 1) {
       return (
         <div className="verse">
-          <span>{this.state.data[0].bookname + ' '+
-                + this.state.data[0].chapter + ':'+
-                this.state.data[0].verse + '-' + this.state.data.length}</span>
+          <span>
+                {this.state.data[0].bookname + ' '+ this.state.data[0].chapter + ':'+
+                 this.state.data[0].verse + '-' + this.state.data.length}
+          </span>
           <br></br>
-          <span> {this.state.data.map((verses,index)=>{return(<span key={index} onClick={()=>this.style={testDecoration: 'underline'}}> <sup>{index + 1 + ' '}</sup>{verses.text}</span>);})}</span>
+          <span> 
+            {this.state.data.map((verses,index)=>{
+                return( 
+                  <span key={index} id={index} className='' 
+                  onClick={()=>this.changeClassName(index)}> 
+                      <sup>{index + 1 + ' '}</sup>
+                      {verses.text}
+                  </span>);
+              })
+            }
+          </span>
       </div>  
       );
       // Single verses case
     } else {
       return this.state.data.map((verse, index) => {
         return (
-        <div key={index} className="verse">
-          <span>{verse.bookname + ' '+
-                + verse.chapter + ':'+
-                verse.verse  }</span>
-          <br></br>
-          <span> {verse.text}</span>
-        </div> 
+          <div key={index} className="verse">
+            <span>
+              {verse.bookname + ' '+verse.chapter + ':'+ verse.verse  }
+            </span>
+            <br></br>
+            <span> {verse.text}</span>
+          </div> 
         );
       });
     }
@@ -92,9 +126,9 @@ export default class App extends Component{
   showButton(buttonName,number){
 
     if(true) {
-      return  (<button id={buttonName} class="flipButton" onClick={()=> this.getVerses(this.state.data[0].bookname,
-      this.state.data[0].chapter-number,
-      '0')}> 
+      return  (
+      <button id={buttonName} className="flipButton" 
+              onClick={()=> this.getVerses(this.state.data[0].bookname,this.state.data[0].chapter-number,'0')}> 
       {buttonName}
       </button>);
       // I wonder if we are on the first page we should not allow to go back but it's more complex than I expected 
@@ -123,16 +157,17 @@ export default class App extends Component{
   }
 
   addTextField(){
+    this.setState({acount: this.state.acount + 1});
     return(
       <div>
-      <input type="text" id="userName" class="textBox" placeholder="enter your name" onChange={ this.setUserName } value={ this.state.name } />
-      <input type="text" id="bookName" class="textBox" placeholder="BookName"/>
+      <input type="text" id="userName" className="textBox" placeholder="enter your name" onChange={ this.setUserName } value={ this.state.name } />
+      <input type="text" id="bookName" className="textBox" placeholder="BookName"/>
       <span> </span>
-      <input type="text" id="chapter" class="textBox" placeholder="chapter"/>
+      <input type="text" id="chapter" className="textBox" placeholder="chapter"/>
       <span>:</span>
-      <input type="text" id="verse" class="textBox" placeholder="verse"/>
+      <input type="text" id="verse" className="textBox" placeholder="verse"/>
 
-      <button id="search" class="button" onClick={()=> this.getVerses(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
+      <button id="search" className="button" onClick={()=> this.getVerses(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
       Search
       </button>
       </div>
@@ -144,45 +179,46 @@ export default class App extends Component{
     if (!this.state.data) return<div>Sorry can't find the verse.</div>;
 
     return (
-      <div class="main">
-         <div class="flip-box" id={this.state.flip}>
-          <div class="flip-box-inner">
-            <div class="flip-box-front"> 
-              <input type="text" id="userName" class="textBox" placeholder="enter your name" onChange={ this.setUserName } value={ this.state.name } />
-              <input type="text" id="bookName" class="textBox" placeholder="BookName"/>
+      <div className="main">
+         <div className="flip-box" id={this.state.flip}>
+          <div className="flip-box-inner">
+            <div className="flip-box-front"> 
+              <input type="text" id="userName" className="textBox" placeholder="enter your name" onChange={ this.setUserName } value={ this.state.name } />
+              <input type="text" id="bookName" className="textBox" placeholder="BookName"/>
               <span> </span>
-              <input type="text" id="chapter" class="textBox" placeholder="chapter"/>
+              <input type="text" id="chapter" className="textBox" placeholder="chapter"/>
               <span>:</span>
-              <input type="text" id="verse" class="textBox" placeholder="verse"/>
+              <input type="text" id="verse" className="textBox" placeholder="verse"/>
       
-              <button id="search" class="button" onClick={()=> this.getVerses(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
+              <button id="search" className="button" onClick={()=> this.getVerses(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
               Search
               </button>
-              <button id="fullChapter" class="button" onClick={()=> this.getVerses(this.state.data[0].bookname,this.state.data[0].chapter,'0')}> 
+              <button id="fullChapter" className="button" onClick={()=> this.getVerses(this.state.data[0].bookname,this.state.data[0].chapter,'0')}> 
               Full Chapter
               </button>
             
-              <button onClick={()=> this.changeFlipState()}>Flip</button>
+              <button className="button" onClick={()=> this.changeFlipState()}>Flip</button>
             </div>
-            <div class="flip-box-back">   
-              <input type="text" id="bookName" class="textBox" placeholder="BookName"/>
+            <div className="flip-box-back">   
+              <input type="text" id="bookName1" className="textBox" placeholder="BookName"/>
               <span> </span>
-              <input type="text" id="chapter" class="textBox" placeholder="chapter"/>
+              <input type="text" id="chapter1" className="textBox" placeholder="chapter"/>
               <span>:</span>
-              <input type="text" id="verse" class="textBox" placeholder="verse"/>
-              
-              <input type="text" id="bookName" class="textBox" placeholder="BookName"/>
+              <input type="text" id="verse1" className="textBox" placeholder="verse"/>
+
+              <input type="text" id="bookName2" className="textBox" placeholder="BookName"/>
               <span> </span>
-              <input type="text" id="chapter" class="textBox" placeholder="chapter"/>
+              <input type="text" id="chapter2" className="textBox" placeholder="chapter"/>
               <span>:</span>
-              <input type="text" id="verse" class="textBox" placeholder="verse"/>
+              <input type="text" id="verse2" className="textBox" placeholder="verse"/>
       
-              <button id="search" class="button" onClick={()=> this.getVerses(document.getElementById("bookName").value,document.getElementById("chapter").value,document.getElementById("verse").value)}> 
+              <button id="search" className="button" onClick={()=> this.getMultipleVerses(document.getElementById("bookName1").value,document.getElementById("chapter1").value,document.getElementById("verse1").value,
+                                                                                      document.getElementById("bookName2").value,document.getElementById("chapter2").value,document.getElementById("verse2").value)}> 
               Search
               </button>
 
-              <button onClick={()=> this.addTextField()}>Add</button>
-              <button onClick={()=> this.changeFlipState()}>Flip</button>
+              <button className="button" onClick={()=> this.addTextField()}>Add</button>
+              <button className="button"onClick={()=> this.changeFlipState()}>Flip</button>
             </div>
           </div>
         </div>
@@ -190,9 +226,11 @@ export default class App extends Component{
         {this.showGreeting()}
         {this.showButton("˂",1)}
         {this.showButton("˃",-1)}
-        <div class="content">{this.showContent()}</div>
+        <div className="content">{this.showContent()}</div>
       </div>
     );
   }
 }
+
+
 
